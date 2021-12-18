8a814c918dc54097d71a3611590f4d952379aff2
Amaury Danis Cousandier <amaury.danis-cousandier@epitech.eu>
2021-12-18 16:55:13 +0100

    organisation

---
+++ src/DisplayImage.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <thread>
+
+
+int main(int argc, char **argv)
+{
+    cv::VideoCapture cameraStream(0);
+    cv::Mat frame;
+    while (1) {
+        if (!cameraStream.isOpened()) {
+            std::cerr << "Cannot open camera stream O_o" << std::endl;
+            break;
+        }
+        cameraStream >> frame;
+        cv::imwrite("/home/frame.jpeg", frame); 
+    }
+    return (0);
+}
